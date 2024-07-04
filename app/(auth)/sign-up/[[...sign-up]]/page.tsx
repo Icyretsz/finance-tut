6643806edd96906c936ff7b36f75b1e0c1008795
Thien <ubw1212@gmail.com>
2024-07-04 22:40:12 +0700

    Update authentication pages and add autoprefixer
    Updated the layout for SignIn and SignUp pages, added loading indicators and custom images for better user experience. Additionally, autoprefixer has been added to the build process to apply vendor prefixes, ensuring better browser compatibility. The PostCSS and TailwindCSS versions have also been updated.

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,30 @@
-import { SignUp } from "@clerk/nextjs";
+import {SignUp, ClerkLoaded, ClerkLoading} from "@clerk/nextjs";
+import {Loader2} from 'lucide-react';
+import Image from 'next/image'
 
 export default function Page() {
-    return <SignUp />;
+    return (
+        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+            <div className="h-full lg:flex flex-col items-center justify-center gap-5">
+                <div className="text-center space-y-4 pt-16">
+                    <h1 className="font-bold text-3xl text-[#2E2A47]">
+                        Create new account
+                    </h1>
+                </div>
+                <div className="flex items-center justify-center mt-8">
+                    <ClerkLoaded>
+                        <SignUp path="/sign-up"/>
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground"></Loader2>
+                    </ClerkLoading>
+                </div>
+            </div>
+            <div>
+                <div className="h-full hidden lg:flex items-center justify-center bg-[#dee1dc]">
+                    <Image src="/logo.webp" height={200} width={200} alt="Login Image"/>
+                </div>
+            </div>
+        </div>
+    )
 }
