9d3bbcf5a10f8b9df523a325a562e0458cc92a53
Thien <ubw1212@gmail.com>
2024-07-05 22:16:34 +0700

    Improve Dashboard Home Component and API authentication
    Updated Dashboard Home component to include a SignInButton and fetch data from '/api/hello'. The /api/hello route has been improved to use 'clerkMiddleware' for authentication, responding differently depending on the user's logged in status. Dependencies in package.json and package-lock.json were also updated to support these changes.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,29 @@
-import { UserButton } from "@clerk/nextjs"
+"use client"
+import {SignInButton, UserButton, SignedIn} from "@clerk/nextjs"
+import  Link  from "next/link"
+import {useEffect, useState} from "react";
 
 export default function Home() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('/api/hello')
+            .then(response => response.json())
+            .then(result => setData(result.message));
+    }, []);
     return (
         <>
-            <UserButton />
+            <UserButton showName={true}/>
+            <SignedIn>
+            <div>
+                {data ? `Message: ${data}` : 'Fetching data...'}
+            </div>
+            </SignedIn>
+            <div className='bg-pink-300 inline-flex p-2 rounded-3xl'>
+                <SignInButton>
+                    <button>Sign in with Clerk</button>
+                </SignInButton>
+            </div>
         </>
     );
 }
--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,23 +1,26 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { z } from "zod"
-import { zValidator } 
+import { zValidator } from '@hono/zod-validator'
+import {clerkMiddleware, getAuth} from "@hono/clerk-auth";
+
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/hello', (c) => {
+app.get('/hello', clerkMiddleware(), (c) => {
+    const auth = getAuth(c)
+    if (!auth?.userId) {
+        return c.json({
+            message: 'You are not logged in.'
+        })
+    }
+
     return c.json({
-        message: 'Hello Next.js!',
+        message: 'You are logged in!',
+        userId: auth.userId
     })
 })
-    .get("/hello/:test", (c) => {
-        const test = c.req.param("test")
-        return c.json({
-            message: "Hi",
-            test: test
-        })
-    })
 
 export const GET = handle(app)
 export const POST = handle(app)
