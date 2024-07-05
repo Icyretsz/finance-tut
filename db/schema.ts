52ce45a22ce1c9c518b6bbaa020ee768b9126316
Thien <ubw1212@gmail.com>
2024-07-06 04:08:07 +0700

    Remove migrate script and update database schema
    The migrate script has been removed and the database schema has been updated with new fields. Files related to migration have been created and the 'drizzle-kit' dependency version has been updated. The schema configuration has also been defined in 'drizzle.config.ts'.

--- db/schema.ts
+++ db/schema.ts
@@ -2,7 +2,22 @@ import {pgTable, text} from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
     id: text("id").primaryKey(),
+    plaidId: text("plaid_id"),
     name: text("name").notNull(),
     userId: text("user_id").notNull()
 })
+//Install dependencies
+// npm install drizzle-orm @neondatabase/serverless
+// npm install -D drizzle-kit dotenv
+//add DATABASE_URL into env file
 
+//create drizzle.ts in db/, contents: https://orm.drizzle.team/docs/get-started-postgresql
+//creating schema: schema.ts in db/, contents: https://orm.drizzle.team/docs/schemas
+//create config: drizzle.config.ts in root, contents: https://orm.drizzle.team/docs/migrations
+//create in package.json scripts:
+// "db:generate": "drizzle-kit generate",
+//     "db:migrate": "drizzle-kit migrate",
+//     "db:studio": "drizzle-kit studio"
+//run in terminal: npm run db:generate to generate migrate script
+//run npm run db:migrate to migrate the schema to neon
+//run npm run db:studio to launch studio
